890604b1c80325b6ac0d683445c122a563576c8c
Jennifer <jennifer.zarazik.93@gmail.com>
2020-10-22 11:23:33 +0200

    es funktioniert

---
+++ database/migrations/2020_10_22_085216_create_entries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title',250);
+            $table->text('description');
+            $table->string('teaser',500);
+            $table->string('img-path',250);
+            $table->foreignId('categorie_id')->constrained('categories');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
